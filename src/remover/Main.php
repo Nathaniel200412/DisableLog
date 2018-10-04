d6417bea93104237d57b771c8eff014dc1aee07a
Assassiner354 <40911019+A354-PH@users.noreply.github.com>
2018-10-05 06:12:08 +0800

    Create Main.php

---
+++ src/remover/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Remover;
+
+use pocketmine\utils\TextFormat as C;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+    public function onEnable() {
+        $this->getLogger()->info(C::GREEN . "Remover has been activated by A354");
+        unlink($this->getServer()->getDataPath()."server.log");
+    }    
+        
+    public function onDisable() {
+        $this->getLogger()->info(C::RED . "Remover has been deactivated by A354");
+    }
+}
