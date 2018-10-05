c0f1633670f7815a74956114082c850efeae8598
Assassiner354 <40911019+A354-PH@users.noreply.github.com>
2018-10-05 15:29:16 +0800

    Remove onRun()

--- src/remover/Main.php
+++ src/remover/Main.php
@@ -10,11 +10,7 @@ class Main extends PluginBase {
     public function onEnable() {
         $this->getLogger()->info(C::GREEN . "Remover has been activated by A354");
         unlink($this->getServer()->getDataPath()."server.log");
-    }    
-    
-    public function onRun($currentTick): void {
-        $this->getLogger()->info(C::BLUE . "Removed the server.log file to avoid storage shortage");
-    }
+    }   
         
     public function onDisable() {
         $this->getLogger()->info(C::RED . "Remover has been deactivated by A354");
