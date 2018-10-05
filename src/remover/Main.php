529b5020b430881b95e412506cf05fdfa4345129
Assassiner354 <40911019+A354-PH@users.noreply.github.com>
2018-10-05 14:42:44 +0800

    Update Main.php

--- src/remover/Main.php
+++ src/remover/Main.php
@@ -11,6 +11,10 @@ class Main extends PluginBase {
         $this->getLogger()->info(C::GREEN . "Remover has been activated by A354");
         unlink($this->getServer()->getDataPath()."server.log");
     }    
+    
+    public function onRun($currentTick): void {
+        $this->getLogger()->info(C::BLUE . "Removed the server.log file to avoid storage shortage");
+    }
         
     public function onDisable() {
         $this->getLogger()->info(C::RED . "Remover has been deactivated by A354");
